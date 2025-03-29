673eccb843d400140f1ac10136fba62dfc329c3a
Muhammad Faheem <mfaheemweb@gmail.com>
2025-03-29 10:04:19 -0700

    add

--- Gsap/script.js
+++ Gsap/script.js
@@ -1,10 +1,12 @@
 var heading =  document.querySelector('.heading');
 
-gsap.from(heading, {
-    duration: 3,
-    x: -100,
-    opacity: 0,
-    scale: 0.5,
-    ease: "bounce",
-    stagger: 0.2
-})
+gsap.fromTo(heading, 
+    // duration: 3,
+    // x: -100,
+    // opacity: 0,
+    // scale: 0.5,
+    // ease: "bounce",
+    // stagger: 0.2,
+    { x: 100, opacity: 1 },
+    { duration: 3, x: 400, opacity: 0 }
+);
